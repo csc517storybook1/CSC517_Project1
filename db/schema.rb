cb5ee6785656e78b74784bfdaa4e5031ef7b8283
Shupeng Niu <sniu@ncsu.edu>
2015-02-25 16:43:22 -0500

    user database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225171535) do
+ActiveRecord::Schema.define(version: 20150225213859) do
 
   create_table "projects", force: :cascade do |t|
     t.text     "Title"
