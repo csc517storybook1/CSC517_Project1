01c92d2b1ce68899b795dcf513a31ca9b3d17a9e
Shupeng Niu <sniu@ncsu.edu>
2015-02-25 18:18:33 -0500

    password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'admins/new'
+
   resources :projects
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225213859) do
+ActiveRecord::Schema.define(version: 20150225231038) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.text     "Title"
